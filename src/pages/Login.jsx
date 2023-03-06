320bb4388c1733e0136b9a3711735c20cbeb99d9
Tons <38555090+tonsisouvanh@users.noreply.github.com>
2023-03-06 14:26:56 +0700

    changed bg color of login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,10 +29,10 @@ const Login = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-slate-800 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div className="text-center">
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
             Sign in to your account
           </h2>
         </div>
