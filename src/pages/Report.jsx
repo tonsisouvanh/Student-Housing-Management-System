f91e65946cab5c08d0835b13d6d09a8665c12c52
Tons <38555090+tonsisouvanh@users.noreply.github.com>
2023-03-07 11:40:45 +0700

    change bg report page to red

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Report = () => {
   return (
-    <div className='dark:bg-slate-600 w-screen h-screen flex items-center justify-center'>
+    <div className='bg-red-600 w-screen h-screen flex items-center justify-center'>
 
       <h1 className='text-white text-6xl'>Report</h1>
     </div>
